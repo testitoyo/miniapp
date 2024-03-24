32990b34387b1a021648fe736f8c3fe52f9d3a38
testitoyo <testitoyo@gmail.com>
2024-03-24 18:04:17 +0330

    hi

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { ScanQr } from 'vue-tg'
+
+function handleScanResult(data: string) {
+  // ...
+}
+</script>
+
+<template>
+  <ScanQr @result="handleScanResult" />
+</template>
